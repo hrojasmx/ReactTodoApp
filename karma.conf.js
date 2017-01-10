0c6e090ff48a58a8d920956aee15a7622452ee1d
Hector Rojas <hrpxa270@hotmail.com>
2017-01-10 00:58:26 -0600

    Add test suite for list component

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,11 @@ module.exports = function (config) {
 
     frameworks: ['mocha'],
 
-    files: ['app/tests/**/*.test.jsx'],
+    files: [
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/foundation-sites/dist/js/foundation.min.js',
+      'app/tests/**/*.test.jsx'
+    ],
 
     preprocessors: {
       'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
